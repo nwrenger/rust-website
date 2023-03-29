b70c6bd2eda374a309ebf1fe3ba9a423a36bac16
NWrenger <nils@wrenger.net>
2023-03-29 14:34:24 +0200

    :rocket: Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use rocket_contrib::templates::Template;
+
+#[derive(serde::Serialize)]
+struct BoardContext {
+    parent: &'static str
+}
+
+#[derive(serde::Serialize)]
+struct AboutContext {
+    parent: &'static str
+}
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", &BoardContext {
+        parent: "layout"
+    })
+}
+
+#[get("/about")]
+fn about() -> Template {
+    Template::render("about", &AboutContext {
+        parent: "layout"
+    })
+}
+
+fn main() {
+    rocket::ignite()
+        .attach(Template::fairing())
+        .mount("/", routes![about])
+        .mount("/", routes![index]).launch();
+}   
