aa80094771fd9afeb3433d7d17504eb1c461636f
NWrenger <nils@wrenger.net>
2023-04-02 19:18:46 +0200

    :wrench: Fixed Format

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@
 extern crate rocket;
 
 use rocket::Request;
-use rocket_dyn_templates::{Template, context};
+use rocket_dyn_templates::{context, Template};
 
-use std::path::{PathBuf, Path};
 use rocket::fs::NamedFile;
+use std::path::{Path, PathBuf};
 
 #[get("/")]
 fn index() -> Template {
-    Template::render("index", context! {
-        parent: "layout",
-        title: "Home",
-    })
+    Template::render(
+        "index",
+        context! {
+            parent: "layout",
+            title: "Home",
+        },
+    )
 }
 
 #[get("/<path..>")]
@@ -23,34 +26,46 @@ pub async fn static_files(path: PathBuf) -> Option<NamedFile> {
 
 #[get("/projects")]
 fn projects() -> Template {
-    Template::render("projects", context! {
-        parent: "layout",
-        title: "My Projects",
-    })
+    Template::render(
+        "projects",
+        context! {
+            parent: "layout",
+            title: "My Projects",
+        },
+    )
 }
 
 #[get("/about")]
 fn about() -> Template {
-    Template::render("about", context! {
-        parent: "layout",
-        title: "About Me",
-    })
+    Template::render(
+        "about",
+        context! {
+            parent: "layout",
+            title: "About Me",
+        },
+    )
 }
 
 #[get("/contact")]
 fn contact() -> Template {
-    Template::render("contact", context! {
-        parent: "layout",
-        title: "Contact",
-    })
+    Template::render(
+        "contact",
+        context! {
+            parent: "layout",
+            title: "Contact",
+        },
+    )
 }
 
 #[get("/legals")]
 fn legals() -> Template {
-    Template::render("legals", context! {
-        parent: "layout",
-        title: "Legals",
-    })
+    Template::render(
+        "legals",
+        context! {
+            parent: "layout",
+            title: "Legals",
+        },
+    )
 }
 
 #[catch(404)]
@@ -69,6 +84,9 @@ fn not_found(req: &Request<'_>) -> Template {
 fn rocket() -> _ {
     rocket::build()
         .register("/", catchers![not_found])
-        .mount("/", routes![index, projects, about, contact, legals, static_files])
+        .mount(
+            "/",
+            routes![index, projects, about, contact, legals, static_files],
+        )
         .attach(Template::fairing())
 }
